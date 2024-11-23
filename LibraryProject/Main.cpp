9ded7f2bf1d8f2179858611266cc7a696d95beb0
Hisan <dwihysan@gmail.com>
2024-11-23 14:59:57 +0700

    Add files via upload

---
+++ LibraryProject/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct siswa{
+   int NIS;
+   string nama;
+   string kelas;
+};
+struct buku{
+   int isbn;
+   string judul;
+   string penerbit;
+   int stok;
+};
+
+int main(){
+   return 0;
+}
+
+
